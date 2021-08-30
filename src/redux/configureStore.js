9386984c87bd49f8f585fde893896e6ff7806501
eroauditore <alfa.fenrir@gmail.com>
2021-08-30 17:51:55 -0500

    fetch data from api rockets

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import {
+  createStore, combineReducers, applyMiddleware, compose,
+} from 'redux';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { rocketsReducer } from './rockets/rockets';
 
 const reducer = combineReducers({
   rocketsReducer,
 });
-
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
