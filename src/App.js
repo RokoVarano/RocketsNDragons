00ed98229ee0ca6b1e0ba4e0a24cce128b3f7a91
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-30 12:44:51 -0400

    Configure APP.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import store from './redux/configureStore';
+
 function App() {
   return (
-    <div className="App" />
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Switch>
+          {/* <Route path="/rockets" component={Rockets} />
+          <Route path="/missions" component={Missions} />
+          <Route path="/dragons" component={Dragons} />
+          <Route path="/" exact component={Profile} /> */}
+          <Route path="/" />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
