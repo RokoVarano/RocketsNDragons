7b85479a23b277a789ffab83301c955611b8ec37
Ricardo <rr155914@gmail.com>
2021-08-31 17:01:17 -0500

    Check linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,10 +5,12 @@ import {
 import thunk from 'redux-thunk';
 import { rocketsReducer } from './rockets/rockets';
 import dragons from './dragons/dragons';
+import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
   dragons,
   rocketsReducer,
+  missions: missionsReducer,
 });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
