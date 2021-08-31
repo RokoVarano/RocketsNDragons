76b8c845736be303e892c245d612a2ab1d28fb96
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-31 17:04:53 -0400

    lift State in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import NavLink from './components/NavLink/NavLink';
-import store from './redux/configureStore';
 import Profile from './components/Profile/Profile';
 import Dragons from './components/Dragons/Dragons';
 import Rockets from './components/Rockets';
+import { fetchRockets } from './redux/rockets/rockets';
+import { getDragons } from './redux/dragons/dragons';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+    dispatch(getDragons());
+  }, []);
+
   return (
-    <Provider store={store}>
-      <Router>
-        <NavLink />
-        <Switch>
-          <Route path="/dragons" component={Dragons} />
-          <Route path="/rockets" component={Rockets} />
-          <Route path="/" exact component={Profile} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <NavLink />
+      <Switch>
+        <Route path="/dragons" component={Dragons} />
+        <Route path="/rockets" component={Rockets} />
+        <Route path="/" exact component={Profile} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Rockets/index.js
+++ src/components/Rockets/index.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
-import { fetchRockets } from '../../redux/rockets/rockets';
 
 import RocketCard from './RocketCard';
 
@@ -17,11 +16,7 @@ const useStyles = makeStyles((theme) => ({
 
 const index = () => {
   const classes = useStyles();
-  const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rocketsReducer.rockets);
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, []);
 
   return (
     <>
