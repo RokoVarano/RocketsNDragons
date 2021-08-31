3172e5a7c1a8f40ff95b56f5709148cc8b0ea7aa
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-31 11:48:20 -0400

    Update Dragon Frontend to match Rockets

--- src/components/Dragons/DragonWidget.jsx
+++ src/components/Dragons/DragonWidget.jsx
@@ -2,51 +2,70 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  },
+  content: {
+    flex: '1 1 auto',
+  },
+  cover: {
+    width: 450,
+  },
+  controls: {
+    display: 'flex',
     alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
   },
-  media: {
-    width: 140,
-    height: 140,
+  playIcon: {
+    height: 38,
+    width: 38,
   },
-});
+}));
 
 const DragonWidget = (props) => {
   const classes = useStyles();
   const { image, name, type } = props;
 
   return (
-
     <Card className={classes.root}>
       <CardMedia
-        className={classes.media}
+        className={classes.cover}
         image={image}
-        title="Dragon"
+        title="Live from space album cover"
       />
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            { name }
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            {name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            { type }
+          <Typography variant="subtitle1" color="textSecondary">
+            {type}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            Reserve
-          </Button>
-        </CardActions>
-      </CardActionArea>
+        <div className={classes.controls}>
+          <CardActions>
+            <Button size="small" color="primary">
+              Reserve Dragon
+            </Button>
+          </CardActions>
+        </div>
+      </div>
     </Card>
   );
 };
