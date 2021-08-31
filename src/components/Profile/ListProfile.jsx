2c33c8f8dba7efff8504043655e77357aeeef61b
eroauditore <alfa.fenrir@gmail.com>
2021-08-31 16:27:32 -0500

    List profile added

---
+++ src/components/Profile/ListProfile.jsx
@@ -0,0 +1,14 @@
+import { List, ListItem, ListItemText } from '@material-ui/core';
+import React from 'react';
+
+const ListProfile = () => {
+  return (
+    <List>
+      <ListItem>
+        <ListItemText primary="un elemento" />
+      </ListItem>
+    </List>
+  );
+};
+
+export default ListProfile;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import ListProfile from './ListProfile';
 
-const Profile = () => (
-  <h2>PROFILE UNDER CONSTRUCTION</h2>
-);
+const useStyles = makeStyles((theme) => ({
+  paperTitle: {
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Profile = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <div className={classes.paperTitle}>
+              <Typography component="h4" variant="h4">
+                My profile
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography component="h4" variant="h4" />
+            <ListProfile />
+          </Grid>
+          <Grid item xs={4}>
+            <Typography component="h4" variant="h4">
+              My Rockets
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography component="h4" variant="h4">
+              My Missions
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
 export default Profile;
