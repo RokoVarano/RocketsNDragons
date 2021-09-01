2df9a8b8939dda2efdd98314b3d736ff4a730467
eroauditore <alfa.fenrir@gmail.com>
2021-09-01 12:34:17 -0500

    Nav logo and cards to profile

--- src/components/Rockets/RocketCard.test.js
+++ src/components/Rockets/RocketCard.test.js
@@ -5,26 +5,26 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import RocketCard from './RocketCard';
 
-
-
-    const initialState = { rockets:[{id:1, rocket_name: "test rocket", description:"The Falcon 1 was", flickr_images:["https://imgur.com/DaCfMsj.jpg","https://imgur.com/azYafd8.jpg"]}]}
-    const mockStore = configureStore();
-    let store;
+const initialState = {
+  rockets: [{
+    id: 1, rocket_name: 'test rocket', description: 'The Falcon 1 was', flickr_images: ['https://imgur.com/DaCfMsj.jpg', 'https://imgur.com/azYafd8.jpg'],
+  }],
+};
+const mockStore = configureStore();
+let store;
 
 describe('Test Button component', () => {
   // Arrange
   store = mockStore(initialState);
-  const data = 
-      { 
-          id:1, 
-          rocket_name: "test rocket", 
-          description:"The Falcon 1 was",
-          flickr_images:["https://imgur.com/DaCfMsj.jpg","https://imgur.com/azYafd8.jpg"] 
-        } ;
+  const data = {
+    id: 1,
+    rocket_name: 'test rocket',
+    description: 'The Falcon 1 was',
+    flickr_images: ['https://imgur.com/DaCfMsj.jpg', 'https://imgur.com/azYafd8.jpg'],
+  };
   // Act
 
   const component = render(<Provider store={store}><RocketCard data={data} /></Provider>);
-  
 
   // Assert
   test('Testing Button component render', () => {
